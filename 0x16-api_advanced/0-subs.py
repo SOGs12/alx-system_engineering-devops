09f0811d5eff27523a62be42fcd9a45bd6d7c979
SOGs12 <mosesoyewumi05@gmail.com>
2024-03-05 07:39:58 -0800

    how many subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Custom User Agent"}
+    headers = {"User-Agent": "Custom SOGs12"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
