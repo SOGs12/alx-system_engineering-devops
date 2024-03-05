bc36255374e421f3a32ea53fdd793833c8ccd5e3
SOGs12 <mosesoyewumi05@gmail.com>
2024-03-05 07:37:26 -0800

    How many subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-'''queries the Reddit API and returns the number of subscribers of subreddit'''
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
 import requests
 
-
 def number_of_subscribers(subreddit):
-    ''' get num of subs '''
-
-    base_url = 'http://reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-agent': 'SOGs12'}
-    res = requests.get(base_url, headers=headers)
-
-    if res.status_code == 200:
-        subs_num = res.json().get('data').get('subscribers')
-        return subs_num
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Custom User Agent"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    elif response.status_code == 302:
+        # This indicates a redirect, which means the subreddit doesn't exist
+        return 0
     else:
+        # Handle other possible errors
+        print("Error:", response.status_code)
         return 0
