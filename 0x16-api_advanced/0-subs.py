5dd1e7399eddae9b30e2707ef35be063d39c5446
SOGs12 <mosesoyewumi05@gmail.com>
2024-03-05 07:21:06 -0800

    How many subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""
-Script that queries subscribers on a given Reddit subreddit.
-"""
-
+'''queries the Reddit API and returns the number of subscribers of subreddit'''
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "SOGs12"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+    ''' get num of subs '''
+
+    base_url = 'http://reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-agent': 'SOGs12'}
+    res = requests.get(base_url, headers=headers)
+
+    if res.status_code == 200:
+        subs_num = res.json().get('data').get('subscribers')
+        return subs_num
     else:
         return 0
