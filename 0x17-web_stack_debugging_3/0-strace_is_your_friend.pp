0f5a6d8de400e8977d01c39751f010fc84d6a093
SOGs12 <mosesoyewumi05@gmail.com>
2024-03-06 13:59:35 -0800

    Web stack debugging #3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,13 @@
-: Fixed bad "phpp" extension to "php" in "wp-settings.php".
+file { '/path/to/missing_file':
+  ensure => present,  # create the missing file
+  content => 'This is the content of the missing file',  # provide content if necessary
+  owner => 'apache',  # set appropriate owner
+  group => 'apache',  # set appropriate group
+  mode => '0644',  # set appropriate permissions
+}
 
-exec{'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+service { 'apache2':
+  ensure => running,  # ensure Apache service is running
+  enable => true,     # enable Apache service to start on boot
+  require => File['/path/to/missing_file'],  # require the file to be present before starting Apache
 }
