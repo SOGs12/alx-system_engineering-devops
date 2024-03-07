33a1da311bce7e34f257c12cf4effe787452fe83
SOGs12 <mosesoyewumi05@gmail.com>
2024-03-06 16:51:18 -0800

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,13 +1,6 @@
-file { '/path/to/missing_file':
-  ensure => present,  # create the missing file
-  content => 'This is the content of the missing file',  # provide content if necessary
-  owner => 'apache',  # set appropriate owner
-  group => 'apache',  # set appropriate group
-  mode => '0644',  # set appropriate permissions
-}
+# Fix 500 error when a GET HTTP method is requested to Apache web server
 
-service { 'apache2':
-  ensure => running,  # ensure Apache service is running
-  enable => true,     # enable Apache service to start on boot
-  require => File['/path/to/missing_file'],  # require the file to be present before starting Apache
+exec {'replace':
+  provider => shell,
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
